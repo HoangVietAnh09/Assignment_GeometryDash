2b06f02facf63bddd25af13da565c1f41333503f
TuanNAb22at <143248046+TuanNAb22at@users.noreply.github.com>
2024-09-20 18:46:12 +0700

    delete

--- src/com/util/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.util;
-
-public class Constants {
-    public static final int SCREEN_WIDTH = 1280;
-    public static final int SCREEN_HEIGHT = 720;
-    public static final String SCREEN_TITLE = "Geometry Dash";
-}
--- src/com/util/Time.java
+++
@@ -1,9 +0,0 @@
-package com.util;
-
-public class Time {
-    public static double timeStarted = System.nanoTime();
-
-    public static double getTimeStarted(){
-        return (System.nanoTime() - timeStarted) * 1E-9;
-    }
-}
