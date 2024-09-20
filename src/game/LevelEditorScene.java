5eaf3344e21822af6ceb03928ff48c81a8d21451
Hoang Viet Anh <vietanh092004@gmail.com>
2024-09-20 10:19:57 +0700

    Add Level Editor Scene file by B22DCAT012

---
+++ src/game/LevelEditorScene.java
@@ -0,0 +1,26 @@
+package game;
+
+import util.Constants;
+
+import java.awt.*;
+
+public class LevelEditorScene extends Scene{
+    public LevelEditorScene(String name) {
+        super.Scene(name);
+    }
+
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void update(double dataTime) {
+    }
+
+    @Override
+    public void draw(Graphics2D g2) {
+        g2.setColor(Color.BLUE);
+        g2.fillRect(0, 0, Constants.SCREEN_WIDTH, Constants.SCREEN_HEIGHT);
+    }
+}
