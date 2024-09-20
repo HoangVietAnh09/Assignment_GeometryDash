9dd500e40d102b353797a91dcae85d2710102dc6
Hoang Viet Anh <vietanh092004@gmail.com>
2024-09-20 10:15:57 +0700

    Add Main file by B22DCAT012

---
+++ src/com/main/Main.java
@@ -0,0 +1,16 @@
+package com.main;
+
+
+import game.Window;
+
+public class Main {
+    public static void main(String[] args) {
+        Window window = Window.getWindow();
+//        window.init();
+
+        //Create a thread to run game
+        Thread mainThread = new Thread(window);
+        mainThread.start();
+
+    }
+}
